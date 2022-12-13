c31eed0e44e62b92b0ed22aecfbf05c160c42524
elikoschwitz <e.koschwitz@googmail.com>
2022-12-13 16:49:31 +0100

    Init: Button to count up and down

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState<number>(0);
+
+    const clickHandlerForAdd = () => {return setCount(count+1)};
+    const clickHandlerForMinus = () => {return setCount(count-1)};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{count}</h1>
+        <button onClick={clickHandlerForAdd} >Drück mich dann wirds mehr!</button>
+        <button onClick={clickHandlerForMinus} >Drück mich dann wirds weniger!</button>
+
       </header>
     </div>
   );
